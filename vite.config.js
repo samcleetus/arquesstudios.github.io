69be420d63e7719366965602aa1e050010d2ad65
Sam Cleetus <iamsamcleetus@gmail.com>
2026-04-19 18:42:59 -0500

    Hopefully fixed issue with icon

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  server: {
+    host: true,
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
